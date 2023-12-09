2e232e0fb915cb1e460055db354fdfb1db0b1e11
syamkumar6 <syamapkl9@gmail.com>
2023-12-09 18:17:43 +0530

    vite config change

--- note-tick/vite.config.js
+++ note-tick/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Set the production mode
+    mode: 'production',
+    // Specify the output directory (adjust as needed)
+    outDir: 'dist',
+    // Enable minification for the production build
+    minify: 'terser',
+    // Enable splitting chunks to improve loading performance
+    chunkSizeWarningLimit: 600,
+  },
+});
